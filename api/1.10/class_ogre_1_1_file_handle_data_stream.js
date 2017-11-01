Tomarron/ogre-1

add 1.10 api docs

api/1.10/class_ogre_1_1_file_handle_data_stream.js
@@ -1,0 +1,27 @@
+var class_ogre_1_1_file_handle_data_stream =
+[
+    [ "AccessMode", "class_ogre_1_1_file_handle_data_stream.html#a81856582e643b4a560e6c3d3ad05e08c", [
+      [ "READ", "class_ogre_1_1_file_handle_data_stream.html#a81856582e643b4a560e6c3d3ad05e08cac5f9d1587e9cc7dd1577af114dcba50e", null ],
+      [ "WRITE", "class_ogre_1_1_file_handle_data_stream.html#a81856582e643b4a560e6c3d3ad05e08ca8e221ca82d405d21d275997fbd3eb900", null ]
+    ] ],
+    [ "FileHandleDataStream", "class_ogre_1_1_file_handle_data_stream.html#af3d8fd17787d46b2e6ab486586e0925f", null ],
+    [ "FileHandleDataStream", "class_ogre_1_1_file_handle_data_stream.html#a16198eae30b14a03c4ebf1a3d2e6e762", null ],
+    [ "~FileHandleDataStream", "class_ogre_1_1_file_handle_data_stream.html#a9b0503929f53ff42daa0eac1847a6c06", null ],
+    [ "close", "class_ogre_1_1_file_handle_data_stream.html#ac6f1032583209f59b503e7f29946230e", null ],
+    [ "eof", "class_ogre_1_1_file_handle_data_stream.html#af1cbf779ef50f4b7439f1f83a4107f6f", null ],
+    [ "getAccessMode", "class_ogre_1_1_file_handle_data_stream.html#a5c83611aecda5358beb6c6e035f33b51", null ],
+    [ "getAsString", "class_ogre_1_1_file_handle_data_stream.html#a022f49c9313deffc5da4383d59d1f32d", null ],
+    [ "getLine", "class_ogre_1_1_file_handle_data_stream.html#a86d2ff5dff63ce4b689dd1d486d3243f", null ],
+    [ "getName", "class_ogre_1_1_file_handle_data_stream.html#a7ebd86825f86b7114024d168e9751395", null ],
+    [ "isReadable", "class_ogre_1_1_file_handle_data_stream.html#a4779f7f57ff90ce12882c7c891651a71", null ],
+    [ "isWriteable", "class_ogre_1_1_file_handle_data_stream.html#a91509b9cec70111c65570883810224be", null ],
+    [ "operator>>", "class_ogre_1_1_file_handle_data_stream.html#a49bfe5809b50735b5ce49e7df9615e5a", null ],
+    [ "read", "class_ogre_1_1_file_handle_data_stream.html#af64e7e826054181f9026e0f71f901c63", null ],
+    [ "readLine", "class_ogre_1_1_file_handle_data_stream.html#aedc53a8cff5334563283a4557d423b25", null ],
+    [ "seek", "class_ogre_1_1_file_handle_data_stream.html#af56e7f07f2f13c2b05772db8ad75201a", null ],
+    [ "size", "class_ogre_1_1_file_handle_data_stream.html#a1d4e9568e53a8071ea9b29a015425aef", null ],
+    [ "skip", "class_ogre_1_1_file_handle_data_stream.html#a379566f8413d0a525fa7d603af6e33d4", null ],
+    [ "skipLine", "class_ogre_1_1_file_handle_data_stream.html#a32966ad5a814f8c72fe0affbefcc6f55", null ],
+    [ "tell", "class_ogre_1_1_file_handle_data_stream.html#a5d4956cb513de300f8d9a3d68ff92e8f", null ],
+    [ "write", "class_ogre_1_1_file_handle_data_stream.html#ad061c86660f6415331df5f8560b46a02", null ]
+];

api/1.10/class_ogre_1_1_hardware_buffer_manager.js
@@ -1,0 +1,35 @@
+var class_ogre_1_1_hardware_buffer_manager =
+[
+    [ "BufferLicenseType", "class_ogre_1_1_hardware_buffer_manager.html#a88f21df1e7ee330033812ddeba4a08fa", [
+      [ "BLT_MANUAL_RELEASE", "class_ogre_1_1_hardware_buffer_manager.html#a88f21df1e7ee330033812ddeba4a08faa372ccf538e8a6022bba092d2b47f4fdb", null ],
+      [ "BLT_AUTOMATIC_RELEASE", "class_ogre_1_1_hardware_buffer_manager.html#a88f21df1e7ee330033812ddeba4a08faa994cf0191de83556e3c1e6cf30cd6acb", null ]
+    ] ],
+    [ "HardwareBufferManager", "class_ogre_1_1_hardware_buffer_manager.html#a01633a02494f92550596f8f261885e6c", null ],
+    [ "~HardwareBufferManager", "class_ogre_1_1_hardware_buffer_manager.html#aabd161897c942b29fd21373427dd8485", null ],
+    [ "_forceReleaseBufferCopies", "class_ogre_1_1_hardware_buffer_manager.html#ab523a5b31c88599812538441b7e37775", null ],
+    [ "_forceReleaseBufferCopies", "class_ogre_1_1_hardware_buffer_manager.html#a0da9a2c9a66030d32c90871191da0b6a", null ],
+    [ "_freeUnusedBufferCopies", "class_ogre_1_1_hardware_buffer_manager.html#a9a2793066da237001972061b76930c95", null ],
+    [ "_notifyConterBufferDestroyed", "class_ogre_1_1_hardware_buffer_manager.html#a720b706017189d8a221b9ee1a6448628", null ],
+    [ "_notifyCounterBufferDestroyed", "class_ogre_1_1_hardware_buffer_manager.html#a45fe3ad0964310d9b150d45eaddf67d3", null ],
+    [ "_notifyIndexBufferDestroyed", "class_ogre_1_1_hardware_buffer_manager.html#aee835c8225ba41a1358db813ce080b0e", null ],
+    [ "_notifyUniformBufferDestroyed", "class_ogre_1_1_hardware_buffer_manager.html#a3c89366be3029b2b7b174a0486cf3c3c", null ],
+    [ "_notifyVertexBufferDestroyed", "class_ogre_1_1_hardware_buffer_manager.html#a2b188dbba89b0210ad7359434aa87bee", null ],
+    [ "_releaseBufferCopies", "class_ogre_1_1_hardware_buffer_manager.html#a8632cd79b8f18ed2b21bd6b474a49276", null ],
+    [ "allocateVertexBufferCopy", "class_ogre_1_1_hardware_buffer_manager.html#a9061909fca1c96d50ddbeeee6df8a4d8", null ],
+    [ "createCounterBuffer", "class_ogre_1_1_hardware_buffer_manager.html#aa950b6d6113207c24cc8c8fc8cc61edf", null ],
+    [ "createIndexBuffer", "class_ogre_1_1_hardware_buffer_manager.html#a90b4266571e1852e2475a63c062623a2", null ],
+    [ "createRenderToVertexBuffer", "class_ogre_1_1_hardware_buffer_manager.html#a875cea867279770d7900f292a8217db6", null ],
+    [ "createUniformBuffer", "class_ogre_1_1_hardware_buffer_manager.html#aa390f967d95c259e39fac0f6d9a17770", null ],
+    [ "createVertexBuffer", "class_ogre_1_1_hardware_buffer_manager.html#adee7ce85fcdeefb8b97b37ece2bab8e4", null ],
+    [ "createVertexBufferBinding", "class_ogre_1_1_hardware_buffer_manager.html#a4d907dd6ed467886ca7df6a188b73d4d", null ],
+    [ "createVertexDeclaration", "class_ogre_1_1_hardware_buffer_manager.html#a0d6a29a55a89d937cec4cbaa6c7e8b4b", null ],
+    [ "destroyVertexBufferBinding", "class_ogre_1_1_hardware_buffer_manager.html#af5abdc6a7e28634b759189e54f1e6fdd", null ],
+    [ "destroyVertexDeclaration", "class_ogre_1_1_hardware_buffer_manager.html#a8df941430aef9a6c87660655b51672a3", null ],
+    [ "getSingleton", "class_ogre_1_1_hardware_buffer_manager.html#a380063fef4a4563c90d45a03ac196dc7", null ],
+    [ "getSingletonPtr", "class_ogre_1_1_hardware_buffer_manager.html#ab633b90f5015dc3199f96e0514347b3b", null ],
+    [ "registerVertexBufferSourceAndCopy", "class_ogre_1_1_hardware_buffer_manager.html#a763b65efa057185d7acbe073bf0e3610", null ],
+    [ "releaseVertexBufferCopy", "class_ogre_1_1_hardware_buffer_manager.html#a34bc652b029f856a5b130f850ba333a0", null ],
+    [ "touchVertexBufferCopy", "class_ogre_1_1_hardware_buffer_manager.html#af83a478657dabe2d2e35f0d876abcc5b", null ],
+    [ "HardwareIndexBufferSharedPtr", "class_ogre_1_1_hardware_buffer_manager.html#ac301d86fde854d92c82283660c10ca58", null ],
+    [ "HardwareVertexBufferSharedPtr", "class_ogre_1_1_hardware_buffer_manager.html#abdb4b01e9cf06263b1bae7fbb0bb2e9e", null ]
+];

api/1.10/class_ogre_1_1_manual_object_factory.js
@@ -1,0 +1,12 @@
+var class_ogre_1_1_manual_object_factory =
+[
+    [ "ManualObjectFactory", "class_ogre_1_1_manual_object_factory.html#ae7ff8d7532887e279d5bc70a022b94a8", null ],
+    [ "~ManualObjectFactory", "class_ogre_1_1_manual_object_factory.html#aada4dab11e551ca13355df254ac0305b", null ],
+    [ "_notifyTypeFlags", "class_ogre_1_1_manual_object_factory.html#aa23e112f39b565fa0b97e60e27014de3", null ],
+    [ "createInstance", "class_ogre_1_1_manual_object_factory.html#a7164ba391aead569e43e5713c7281d7b", null ],
+    [ "destroyInstance", "class_ogre_1_1_manual_object_factory.html#abcaadec45f403cb7d358fdb9e519de25", null ],
+    [ "getType", "class_ogre_1_1_manual_object_factory.html#a0a97ac9f90a08a79b411bb6577ce3ac4", null ],
+    [ "getTypeFlags", "class_ogre_1_1_manual_object_factory.html#a9b2028dca564c74453994926db4f50d0", null ],
+    [ "requestTypeFlags", "class_ogre_1_1_manual_object_factory.html#a80346bc406655855ec5e0eef198900f2", null ],
+    [ "FACTORY_TYPE_NAME", "class_ogre_1_1_manual_object_factory.html#a17fec4f461f88f160ddf4d049fb3b604", null ]
+];

mimon/ogre

api/1.10/class_ogre_1_1_file_handle_data_stream.js
@@ -1,0 +1,27 @@
+var class_ogre_1_1_file_handle_data_stream =
+[
+    [ "AccessMode", "class_ogre_1_1_file_handle_data_stream.html#a81856582e643b4a560e6c3d3ad05e08c", [
+      [ "READ", "class_ogre_1_1_file_handle_data_stream.html#a81856582e643b4a560e6c3d3ad05e08cac5f9d1587e9cc7dd1577af114dcba50e", null ],
+      [ "WRITE", "class_ogre_1_1_file_handle_data_stream.html#a81856582e643b4a560e6c3d3ad05e08ca8e221ca82d405d21d275997fbd3eb900", null ]
+    ] ],
+    [ "FileHandleDataStream", "class_ogre_1_1_file_handle_data_stream.html#af3d8fd17787d46b2e6ab486586e0925f", null ],
+    [ "FileHandleDataStream", "class_ogre_1_1_file_handle_data_stream.html#a16198eae30b14a03c4ebf1a3d2e6e762", null ],
+    [ "~FileHandleDataStream", "class_ogre_1_1_file_handle_data_stream.html#a9b0503929f53ff42daa0eac1847a6c06", null ],
+    [ "read", "class_ogre_1_1_file_handle_data_stream.html#af64e7e826054181f9026e0f71f901c63", null ],
+    [ "write", "class_ogre_1_1_file_handle_data_stream.html#ad061c86660f6415331df5f8560b46a02", null ],
+    [ "skip", "class_ogre_1_1_file_handle_data_stream.html#a379566f8413d0a525fa7d603af6e33d4", null ],
+    [ "seek", "class_ogre_1_1_file_handle_data_stream.html#af56e7f07f2f13c2b05772db8ad75201a", null ],
+    [ "tell", "class_ogre_1_1_file_handle_data_stream.html#a5d4956cb513de300f8d9a3d68ff92e8f", null ],
+    [ "eof", "class_ogre_1_1_file_handle_data_stream.html#af1cbf779ef50f4b7439f1f83a4107f6f", null ],
+    [ "close", "class_ogre_1_1_file_handle_data_stream.html#ac6f1032583209f59b503e7f29946230e", null ],
+    [ "getName", "class_ogre_1_1_file_handle_data_stream.html#a7ebd86825f86b7114024d168e9751395", null ],
+    [ "getAccessMode", "class_ogre_1_1_file_handle_data_stream.html#a5c83611aecda5358beb6c6e035f33b51", null ],
+    [ "isReadable", "class_ogre_1_1_file_handle_data_stream.html#a4779f7f57ff90ce12882c7c891651a71", null ],
+    [ "isWriteable", "class_ogre_1_1_file_handle_data_stream.html#a91509b9cec70111c65570883810224be", null ],
+    [ "operator>>", "class_ogre_1_1_file_handle_data_stream.html#a49bfe5809b50735b5ce49e7df9615e5a", null ],
+    [ "readLine", "class_ogre_1_1_file_handle_data_stream.html#aedc53a8cff5334563283a4557d423b25", null ],
+    [ "getLine", "class_ogre_1_1_file_handle_data_stream.html#a86d2ff5dff63ce4b689dd1d486d3243f", null ],
+    [ "getAsString", "class_ogre_1_1_file_handle_data_stream.html#a022f49c9313deffc5da4383d59d1f32d", null ],
+    [ "skipLine", "class_ogre_1_1_file_handle_data_stream.html#a32966ad5a814f8c72fe0affbefcc6f55", null ],
+    [ "size", "class_ogre_1_1_file_handle_data_stream.html#a1d4e9568e53a8071ea9b29a015425aef", null ]
+];

api/1.10/class_ogre_1_1_hardware_buffer_manager.js
@@ -1,0 +1,33 @@
+var class_ogre_1_1_hardware_buffer_manager =
+[
+    [ "BufferLicenseType", "class_ogre_1_1_hardware_buffer_manager.html#a88f21df1e7ee330033812ddeba4a08fa", [
+      [ "BLT_MANUAL_RELEASE", "class_ogre_1_1_hardware_buffer_manager.html#a88f21df1e7ee330033812ddeba4a08faa372ccf538e8a6022bba092d2b47f4fdb", null ],
+      [ "BLT_AUTOMATIC_RELEASE", "class_ogre_1_1_hardware_buffer_manager.html#a88f21df1e7ee330033812ddeba4a08faa994cf0191de83556e3c1e6cf30cd6acb", null ]
+    ] ],
+    [ "HardwareBufferManager", "class_ogre_1_1_hardware_buffer_manager.html#a01633a02494f92550596f8f261885e6c", null ],
+    [ "~HardwareBufferManager", "class_ogre_1_1_hardware_buffer_manager.html#aabd161897c942b29fd21373427dd8485", null ],
+    [ "createVertexBuffer", "class_ogre_1_1_hardware_buffer_manager.html#adee7ce85fcdeefb8b97b37ece2bab8e4", null ],
+    [ "createIndexBuffer", "class_ogre_1_1_hardware_buffer_manager.html#a90b4266571e1852e2475a63c062623a2", null ],
+    [ "createRenderToVertexBuffer", "class_ogre_1_1_hardware_buffer_manager.html#a875cea867279770d7900f292a8217db6", null ],
+    [ "createUniformBuffer", "class_ogre_1_1_hardware_buffer_manager.html#aa390f967d95c259e39fac0f6d9a17770", null ],
+    [ "createCounterBuffer", "class_ogre_1_1_hardware_buffer_manager.html#aa950b6d6113207c24cc8c8fc8cc61edf", null ],
+    [ "createVertexDeclaration", "class_ogre_1_1_hardware_buffer_manager.html#a0d6a29a55a89d937cec4cbaa6c7e8b4b", null ],
+    [ "destroyVertexDeclaration", "class_ogre_1_1_hardware_buffer_manager.html#a8df941430aef9a6c87660655b51672a3", null ],
+    [ "createVertexBufferBinding", "class_ogre_1_1_hardware_buffer_manager.html#a4d907dd6ed467886ca7df6a188b73d4d", null ],
+    [ "destroyVertexBufferBinding", "class_ogre_1_1_hardware_buffer_manager.html#af5abdc6a7e28634b759189e54f1e6fdd", null ],
+    [ "registerVertexBufferSourceAndCopy", "class_ogre_1_1_hardware_buffer_manager.html#a763b65efa057185d7acbe073bf0e3610", null ],
+    [ "allocateVertexBufferCopy", "class_ogre_1_1_hardware_buffer_manager.html#a9061909fca1c96d50ddbeeee6df8a4d8", null ],
+    [ "releaseVertexBufferCopy", "class_ogre_1_1_hardware_buffer_manager.html#a34bc652b029f856a5b130f850ba333a0", null ],
+    [ "touchVertexBufferCopy", "class_ogre_1_1_hardware_buffer_manager.html#af83a478657dabe2d2e35f0d876abcc5b", null ],
+    [ "_freeUnusedBufferCopies", "class_ogre_1_1_hardware_buffer_manager.html#a9a2793066da237001972061b76930c95", null ],
+    [ "_releaseBufferCopies", "class_ogre_1_1_hardware_buffer_manager.html#a8632cd79b8f18ed2b21bd6b474a49276", null ],
+    [ "_forceReleaseBufferCopies", "class_ogre_1_1_hardware_buffer_manager.html#ab523a5b31c88599812538441b7e37775", null ],
+    [ "_forceReleaseBufferCopies", "class_ogre_1_1_hardware_buffer_manager.html#a0da9a2c9a66030d32c90871191da0b6a", null ],
+    [ "_notifyVertexBufferDestroyed", "class_ogre_1_1_hardware_buffer_manager.html#a2b188dbba89b0210ad7359434aa87bee", null ],
+    [ "_notifyIndexBufferDestroyed", "class_ogre_1_1_hardware_buffer_manager.html#aee835c8225ba41a1358db813ce080b0e", null ],
+    [ "_notifyUniformBufferDestroyed", "class_ogre_1_1_hardware_buffer_manager.html#a3c89366be3029b2b7b174a0486cf3c3c", null ],
+    [ "_notifyConterBufferDestroyed", "class_ogre_1_1_hardware_buffer_manager.html#a720b706017189d8a221b9ee1a6448628", null ],
+    [ "getSingleton", "class_ogre_1_1_hardware_buffer_manager.html#a380063fef4a4563c90d45a03ac196dc7", null ],
+    [ "getSingletonPtr", "class_ogre_1_1_hardware_buffer_manager.html#ab633b90f5015dc3199f96e0514347b3b", null ],
+    [ "_notifyCounterBufferDestroyed", "class_ogre_1_1_hardware_buffer_manager.html#a45fe3ad0964310d9b150d45eaddf67d3", null ]
+];

api/1.10/class_ogre_1_1_manual_object_factory.js
@@ -1,0 +1,12 @@
+var class_ogre_1_1_manual_object_factory =
+[
+    [ "ManualObjectFactory", "class_ogre_1_1_manual_object_factory.html#ae7ff8d7532887e279d5bc70a022b94a8", null ],
+    [ "~ManualObjectFactory", "class_ogre_1_1_manual_object_factory.html#aada4dab11e551ca13355df254ac0305b", null ],
+    [ "getType", "class_ogre_1_1_manual_object_factory.html#a0a97ac9f90a08a79b411bb6577ce3ac4", null ],
+    [ "destroyInstance", "class_ogre_1_1_manual_object_factory.html#abcaadec45f403cb7d358fdb9e519de25", null ],
+    [ "createInstance", "class_ogre_1_1_manual_object_factory.html#a23f425e96487a07c316b2e828ee3ed2f", null ],
+    [ "requestTypeFlags", "class_ogre_1_1_manual_object_factory.html#a80346bc406655855ec5e0eef198900f2", null ],
+    [ "_notifyTypeFlags", "class_ogre_1_1_manual_object_factory.html#aa23e112f39b565fa0b97e60e27014de3", null ],
+    [ "getTypeFlags", "class_ogre_1_1_manual_object_factory.html#a9b2028dca564c74453994926db4f50d0", null ],
+    [ "FACTORY_TYPE_NAME", "class_ogre_1_1_manual_object_factory.html#a17fec4f461f88f160ddf4d049fb3b604", null ]
+];